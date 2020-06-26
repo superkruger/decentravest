5f555cf3e18c36bce3d51c0dce2cb7fa3921d7d7
Jaco Kruger <superkruger@gmail.com>
2020-06-26 15:12:18 +0200

    mainnet config

--- networks.js
+++ networks.js
@@ -26,6 +26,19 @@ module.exports = {
       gas: 5000000,
       gasPrice: 25000000000,
       network_id: 3
+    },
+    mainnet: {
+      provider: function() {
+        return new HDWalletProvider(
+          privateKeys.split(','), // Array of account private keys
+          `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,// Url to an Ethereum Node
+          0,
+          2
+        )
+      },
+      gas: 5000000,
+      gasPrice: 30000000000,
+      network_id: 1
     }
   }
 };
